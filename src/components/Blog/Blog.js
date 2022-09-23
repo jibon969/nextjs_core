f24c7da8939406091c04045921359ef44f7bb463
jibon969 <jayed.swe@gmail.com>
2022-09-24 00:58:08 +0600

    added blog detail page fetch api

--- src/components/Blog/Blog.js
+++ src/components/Blog/Blog.js
@@ -1,7 +1,7 @@
 import Link from 'next/link'
 import Image from 'next/image'
 
-const Blog = () => {
+const Blog = ({posts}) => {
     return (
         <div>
             <div className="container my-5">
@@ -12,106 +12,37 @@ const Blog = () => {
                     <div className="col-md-3 mb-3">
                         <div className="search">
                             <form action="#">
-                                <input type="text" placeholder="Search Here ..." className="form-control" />
+                                <input type="text" placeholder="Search Here ..." className="form-control"/>
                             </form>
                         </div>
                     </div>
                 </div>
                 <div className="row">
-                    <div className="col-md-4 mb-4">
-                        <div className="card">
-                            <div className="card-body">
-                                <h3>Hello</h3>
-                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error, inventore.</p>
-                            </div>
-                            <div className="card-footer">
-                                <div className="text-center">
-                                    <Link href="/blog/slug">
-                                        <a>More Details</a>
-                                    </Link>
+                    {
+                        posts.results.map((post) => (
+                            <div className="col-md-4 mb-4">
+                                <div className="card">
+                                    <img src={post.image} style={{width: "100%", height: 250}} className="img-responsive"/>
+                                    <div className="card-body">
+                                        <h5>{post.title}</h5>
+                                        <p>{post.description}</p>
+                                    </div>
+                                    <div className="card-footer">
+                                        <div className="text-center">
+                                            <Link href={`blog/${post.id}`}>
+                                                <a>More Details</a>
+                                            </Link>
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                    </div>
-                    <div className="col-md-4 mb-4">
-                        <div className="card">
-                            <div className="card-body">
-                                <h3>Hello</h3>
-                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error, inventore.</p>
-                            </div>
-                            <div className="card-footer">
-                                <div className="text-center">
-                                    <Link href="/blog/slug">
-                                        <a>More Details</a>
-                                    </Link>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="col-md-4 mb-4">
-                        <div className="card">
-                            <div className="card-body">
-                                <h3>Hello</h3>
-                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error, inventore.</p>
-                            </div>
-                            <div className="card-footer">
-                                <div className="text-center">
-                                    <Link href="/blog/slug">
-                                        <a>More Details</a>
-                                    </Link>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="col-md-4 mb-4">
-                        <div className="card">
-                            <div className="card-body">
-                                <h3>Hello</h3>
-                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error, inventore.</p>
-                            </div>
-                            <div className="card-footer">
-                                <div className="text-center">
-                                    <Link href="/blog/slug">
-                                        <a>More Details</a>
-                                    </Link>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="col-md-4 mb-4">
-                        <div className="card">
-                            <div className="card-body">
-                                <h3>Hello</h3>
-                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error, inventore.</p>
-                            </div>
-                            <div className="card-footer">
-                                <div className="text-center">
-                                    <Link href="/blog/slug">
-                                        <a>More Details</a>
-                                    </Link>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="col-md-4 mb-4">
-                        <div className="card">
-                            <div className="card-body">
-                                <h3>Hello</h3>
-                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error, inventore.</p>
-                            </div>
-                            <div className="card-footer">
-                                <div className="text-center">
-                                    <Link href="/blog/slug">
-                                        <a>More Details</a>
-                                    </Link>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+                        ))
+                    }
+
                 </div>
                 <div className="row">
                     <div className="col">
-                        <nav aria-label="Page navigation example" style={{float:"right"}}>
+                        <nav aria-label="Page navigation example" style={{float: "right"}}>
                             <ul className="pagination">
                                 <li className="page-item"><a className="page-link" href="#">Previous</a></li>
                                 <li className="page-item"><a className="page-link" href="#">1</a></li>
--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -1,11 +1,24 @@
 import Blog from '../../components/Blog/Blog'
 
-const BlogPage = () => {
+const BlogPage = ({posts}) => {
+    console.log("Post:", posts);
     return (
         <div>
-            <Blog/>
+            <Blog posts={posts} />
         </div>
     )
 };
+
+
+export async function getStaticProps() {
+    const res = await fetch('http://127.0.0.1:8000/blog/');
+    const posts = await res.json();
+    return {
+        props: {
+            posts,
+        },
+    }
+}
+
 
 export default BlogPage;
