a2a520d81250846aafa8d748cb54d2f01813510c
jayed969 <jayed.swe@gmail.com>
2022-10-19 23:55:48 +0600

    added empoyee page

--- src/pages/employee/index.js
+++ src/pages/employee/index.js
@@ -1,9 +1,20 @@
+// import react from 'react'
 
 const Employee = () => {
+
+
+  
+
+    const getEmployee = () => {
+        console.log("Hello Function ");
+        const employee = fetch('http://127.0.0.1:8000/blog/');
+        console.log("Employee :", employee)
+    };
+
     return (
         <div>
             <div className="container mt-3">
-                <h2 className="text-center">Hello employee</h2>
+                <h2 className="text-center" onClick={getEmployee}>Hello employee</h2>
                 <hr/>
                 <div className="row">
                     <div className="col">
