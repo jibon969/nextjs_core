a5d41a17aadcd97ead8325b3f5a84870c26072c8
jayed969 <jayed.swe@gmail.com>
2022-10-26 23:10:35 +0600

    added blog

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -1,7 +1,26 @@
+import React, {useState, useEffect} from 'react';
 
+const getBlogData = () => {
+    const [data, setData] = useState(null);
 
-const Index = () =>{
-    return(
+    fetch('http://127.0.0.1:8000/blog/')
+        .then((res) => {
+            return res.json()
+        })
+        .then((data) => {
+            setData(data)
+        })
+        .catch((errors) => {
+            console.log("Errors", errors)
+        })
+};
+
+useEffect(() => {
+    getBlogData()
+}, []);
+
+const Index = () => {
+    return (
         <div>
             <h3>Hello</h3>
         </div>
