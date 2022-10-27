1669547511b2c9cf02a18351257fe40641cc231c
jayed969 <jayed.swe@gmail.com>
2022-10-27 23:58:21 +0600

    added blog fetch data

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -1,29 +1,58 @@
 import React, {useState, useEffect} from 'react';
-import Blog from '../../components/Blog/Blog';
+import BlogList from '../../components/Blog/BlogList';
 
-const getBlogData = () => {
-    const [blog, setBlog] = useState(null);
-
-    fetch('http://127.0.0.1:8000/blog/')
-        .then((res) => {
-            return res.json()
-        })
-        .then((data) => {
-            setBlog(data)
-        })
-        .catch((errors) => {
-            console.log("Errors", errors)
-        })
-};
-
-useEffect(() => {
-    getBlogData()
-}, []);
 
 const Index = () => {
+    // const getBlogData = () => {
+    //     const [blog, setBlog] = useState(null);
+    //
+    //     fetch('http://127.0.0.1:8000/blog/')
+    //         .then((res) => {
+    //             return res.json()
+    //         })
+    //         .then((data) => {
+    //             setBlog(data)
+    //         })
+    //         .catch((errors) => {
+    //             console.log("Errors", errors)
+    //         })
+    // };
+    //
+    // useEffect(() => {
+    //     getBlogData()
+    // }, []);
+
+    const [blogData, setBlogData] = useState(null);
+    const [isLoading, setIsLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+    const getBlogList = () => {
+        fetch('http://127.0.0.1:8000/blog/')
+            .then((res) => {
+                if (!res.ok) {
+                    throw Error("Fetching is not successful")
+                }
+                else {
+                    return res.json()
+                }
+            })
+            .then((data) => {
+                setBlogData(data);
+                setIsLoading(false);
+            })
+            .catch((error) => {
+                setError(error.message);
+                setIsLoading(false);
+            })
+    };
+
+    useEffect(() => {
+        getBlogList()
+    }, []);
+
     return (
         <div>
-            <Blog />
+            <BlogList blogData={blogData}/>
         </div>
     )
 };
