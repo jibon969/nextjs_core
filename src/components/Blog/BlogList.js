e748c0367547005d95a3c44f372ee48410f07103
jayed969 <jayed.swe@gmail.com>
2022-10-28 22:06:15 +0600

    added map data in blog

--- src/components/Blog/BlogList.js
+++ src/components/Blog/BlogList.js
@@ -4,21 +4,25 @@ const BlogList = ({blogData}) => {
         <div>
             <div className="container mt-4">
                 <div className="row">
-                    <div className="col-md-4">
-                        <div className="card">
-                            <img src="" className="card-img-top" alt="..."/>
-                                <div className="card-body">
-                                    <h5 className="card-title">Card title</h5>
-                                    <p className="card-text">Some quick example text to build on the card title and make
-                                        up the bulk of the card's content.</p>
-                                    <a href="#" className="btn btn-primary">Go somewhere</a>
+                    {
+                        blogData && blogData((data, index) => (
+                            <div className="col-md-4" key={index}>
+                                <div className="card">
+                                    <img src="" className="card-img-top" alt="..."/>
+                                    <div className="card-body">
+                                        <h5 className="card-title">{data.title}</h5>
+                                        <p className="card-text">{data.title}</p>
+                                        <a href="#" className="btn btn-primary">Go somewhere</a>
+                                    </div>
                                 </div>
-                        </div>
-                    </div>
+                            </div>
+                        ))
+                    }
+
                 </div>
             </div>
         </div>
-)
+    )
 };
 
 export default BlogList;
