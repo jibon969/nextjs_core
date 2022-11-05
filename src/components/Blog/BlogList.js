d41c296728bc505e9c44d110eb1f0deba341ae52
jayed969 <jayed.swe@gmail.com>
2022-11-05 22:00:19 +0600

    added blog details

--- src/components/Blog/BlogList.js
+++ src/components/Blog/BlogList.js
@@ -1,4 +1,3 @@
-
 const BlogList = ({blogData}) => {
     return (
         <div>
@@ -6,8 +5,8 @@ const BlogList = ({blogData}) => {
                 <h3 className="text-center">Blog</h3>
                 <hr/>
                 <div className="row">
-                    <div className="col-md-3"/>
-                    <div className="col-md-9">
+                    <div className="col-md-9"/>
+                    <div className="col-md-3">
                         <div className="float-right">
                             <input type="text" placeholder="Search" className="form-control"/>
                         </div>
@@ -24,13 +23,29 @@ const BlogList = ({blogData}) => {
                                         <h5 className="card-title">{data.title}</h5>
                                         <p className="card-text">{data.title}</p>
                                         <div className="d-grid">
-                                            <button type="button" className="btn btn-secondary btn-block">Read More</button>
+                                            <button type="button" className="btn btn-secondary btn-block">Read More
+                                            </button>
                                         </div>
                                     </div>
                                 </div>
                             </div>
                         ))
                     }
+                </div>
+                <div className="row">
+                    <div className="col">
+                        <div className="float-end">
+                            <nav aria-label="Page navigation example">
+                                <ul className="pagination">
+                                    <li className="page-item"><a className="page-link" href="#">Previous</a></li>
+                                    <li className="page-item"><a className="page-link" href="#">1</a></li>
+                                    <li className="page-item"><a className="page-link" href="#">2</a></li>
+                                    <li className="page-item"><a className="page-link" href="#">3</a></li>
+                                    <li className="page-item"><a className="page-link" href="#">Next</a></li>
+                                </ul>
+                            </nav>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
