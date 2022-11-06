f311921efdefd502be340fc9695d862bfd52bb95
jayed969 <jayed.swe@gmail.com>
2022-11-06 23:50:37 +0600

    added blog detail id

---
+++ src/pages/blog/[id].js
@@ -0,0 +1,25 @@
+import {useRouter} from "next/router";
+
+const BlogDetail = () => {
+    const router = useRouter();
+
+    console.log(router.query);
+    return (
+        <div>
+            <div className="container mt-3">
+                <h3 className="text-center">Blog Detail</h3>
+                <hr/>
+                <div className="row">
+                    <div className="col-md-8">
+                        <h4>Hello</h4>
+                    </div>
+                    <div className="col-md-4">
+                        <h4>Related Post</h4>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+};
+
+export default BlogDetail;
