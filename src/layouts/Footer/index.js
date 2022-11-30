8190f762a5ed391b008b8e5adbf4c65ad9c59bb6
jayed969 <jayed.swe@gmail.com>
2022-11-30 11:23:21 +0600

    remove some text

--- src/layouts/Footer/index.js
+++ src/layouts/Footer/index.js
@@ -79,13 +79,13 @@ const IndexPage = () => {
                             </div>
                             <div className="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
                                 <h6 className="text-uppercase fw-bold mb-4">Contact</h6>
-                                <p><i className="fa fa-home me-3 text-secondary"/> New York, NY 10012, US</p>
+                                <p><i className="fa fa-home me-3 text-secondary"/> Dhaka, Bangladesh</p>
                                 <p>
                                     <i className="fa fa-envelope me-3 text-secondary"/>
                                     info@example.com
                                 </p>
-                                <p><i className="fa fa-phone me-3 text-secondary"/> + 01 234 567 88</p>
-                                <p><i className="fa fa-print me-3 text-secondary"/> + 01 234 567 89</p>
+                                <p><i className="fa fa-phone me-3 text-secondary"/> + 01987132107</p>
+                                <p><i className="fa fa-print me-3 text-secondary"/> + 01987132107</p>
                             </div>
 
                         </div>
