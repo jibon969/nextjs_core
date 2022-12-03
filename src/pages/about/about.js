d2d534bb4ad828a95bf298e4cd29d7fdd5d0cd07
jayed969 <jayed.swe@gmail.com>
2022-12-03 22:25:43 +0600

    added about js

---
+++ src/pages/about/about.js
@@ -0,0 +1,19 @@
+const AboutPage = () => {
+    return(
+        <div>
+            <div className="container">
+                <div className="row">
+                    <h4 className="text-center">About Page</h4>
+                    <hr/>
+                    <div className="col mb-4">
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime, nisi.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+};
+
+export default AboutPage;
