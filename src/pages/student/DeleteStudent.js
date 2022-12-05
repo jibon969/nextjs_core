5edf101204e3621de3d89d842844b070e8749e6a
jayed969 <jayed.swe@gmail.com>
2022-12-05 23:19:13 +0600

    added delete student

---
+++ src/pages/student/DeleteStudent.js
@@ -0,0 +1,23 @@
+const DeleteStudent = () => {
+    return (
+        <div>
+            <div className="container">
+                <h3 className="text-center">Delete Student</h3>
+                <hr/>
+                <div className="row">
+                    <div className="col">
+                        <div className="card">
+                            <div className="card-body">
+                                <h4>Delete Student</h4>
+
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+};
+
+export default DeleteStudent;
